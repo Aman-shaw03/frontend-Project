add08da3fd24e3873bf301a595cff7927882ac23
Aman shaw <amanshawwork@gmail.com>
2024-11-01 23:30:37 +0530

    setup files in root directory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      "/api": "http://localhost:9000/api/v1",
+      // now every api request that hit on /api will be redirect to this localhost 9000
+    }
+  }
 })
