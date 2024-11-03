17aaccd0fec55c710a959d2d191074df0eb8f8b1
Aman shaw <amanshawwork@gmail.com>
2024-11-03 23:34:55 +0530

    create a Axios instance

---
+++ src/helpers/axios.helper.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+
+const baseURL = "http://localhost:8000/api/v1";
+
+export const axiosInstance = axios.create({
+    baseURL,
+    withCredentials: true
+})
+
+// made a general instance of axios , so it can be used in different times with get post and endpoint
+// i have set the base URL port as 8000 as its in BE , No risk
