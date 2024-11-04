e10edcad43c21837ca865316c59d079beac53dc0
Aman shaw <amanshawwork@gmail.com>
2024-11-04 14:54:19 +0530

    write code for formatFigure file

--- src/helpers/axios.helper.js
+++ src/helpers/axios.helper.js
@@ -10,3 +10,5 @@ export const axiosInstance = axios.create({
 
 // made a general instance of axios , so it can be used in different times with get post and endpoint
 // i have set the base URL port as 8000 as its in BE , No risk
+
+// console.log(Date.now())
