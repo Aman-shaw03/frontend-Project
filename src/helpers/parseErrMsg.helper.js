0b0be6650e3489336979a74110e3355393037ff9
Aman shaw <amanshawwork@gmail.com>
2024-11-04 15:15:49 +0530

    write parse Error message code

---
+++ src/helpers/parseErrMsg.helper.js
@@ -0,0 +1,21 @@
+export const parseErrorMessage = (responseHTMLString) => {
+    const parser = new DOMParser()
+    const responseDocument = parser.parseFromString(responseHTMLString, "text/html")
+    const errorMessageElement = responseDocument.querySelector("pre")
+    //Searches the parsed HTML document for the first <pre> element. Often, error messages are displayed within <pre> tags in HTML responses.
+
+    if(errorMessageElement){
+        const errorMessage = errorMessageElement.textContent.match(/^Error:\s*(.*?)(?=\s*at)/)
+        if(errorMessage && errorMessage[1]){
+            return errorMessage[1].trim()
+        }
+    }
+    return "Something went wrong 😕";
+
+
+    /*The regular expression matches a string that starts with "Error:", followed by any characters (non-greedy) 
+    until the first occurrence of "at" (indicating a stack trace).
+    The first element of the returned array (errorMessage[0]) is the entire matched string. The subsequent 
+    elements, if any, correspond to the captured groups within the regular expression. In our case, the first
+    captured group (errorMessage[1]) contains the specific error message we're interested in. */
+}
