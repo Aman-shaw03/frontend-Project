2652b2af21c526c357e3655c7a803b0d58793dc3
Aman shaw <amanshawwork@gmail.com>
2024-11-14 14:18:08 +0530

    write code for Input

--- src/components/Atoms/Input.jsx
+++ src/components/Atoms/Input.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useId} from 'react'
 
-function Input() {
+function Input(
+  {
+    label,
+    required = false,
+    type = "text",
+    className = "",
+    labelClassName = "",
+    ...props
+  }, ref
+) {
+  const id = useId()
+
   return (
-    <div>Input</div>
+    <div>
+      {label && (
+        <label
+         htmlFor={id}
+         className={`mb-1 inline-block dark:text-gray-300 text-black ${labelClassName}`}
+        >
+          {required && <span className=" text-red-600 text-l">* </span>}
+          {label}
+        </label>
+      )}
+      <input
+       type={type}
+       id={id}
+       ref={ref}
+       className={`rounded-lg border bg-transparent px-3 py-2 ${className}`}
+       {...props} />
+    </div>
   )
+
+  
+  
 }
 
-export default Input
+export default React.forwardRef(Input) // so parent component can access the "Input" component Dom node
