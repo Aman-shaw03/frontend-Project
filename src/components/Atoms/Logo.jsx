14bb0fd585788e59c5c0e587c84d28dd34b4ec13
Aman shaw <amanshawwork@gmail.com>
2024-11-15 15:09:11 +0530

    Atom controller code

--- src/components/Atoms/Logo.jsx
+++ src/components/Atoms/Logo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
-function Logo() {
+import {Link} from "react-router-dom"
+import some from "../../assets/some.png"
+function Logo({
+  width = "w-12 sm:w-16 ", 
+  className = "" 
+}) {
   return (
-    <div>Logo</div>
-  )
+    <Link to={"/"}>
+      <div className={`mr-4 ${width} shrink-0 ${className}`}>
+        <img src={some} alt="" className="w-12 rounded  h-18" />
+      </div>
+    </Link>
+  );
 }
 
 export default Logo
